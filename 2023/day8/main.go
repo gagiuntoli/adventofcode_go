60961eb43636df296b7f094de9b6bbaf47681fdb
Guido Giuntoli <gagiuntoli@gmail.com>
2023-12-08 11:04:20 +0100

    Day 8 - 2023

---
+++ 2023/day8/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Direction struct {
+	left  string
+	right string
+}
+
+func follow_instructions(directions map[string]Direction, instructions string, start string) (int, string) {
+	var destiny string
+
+	for i, c := range instructions {
+		destiny = apply_step(directions, byte(c), start)
+		start = destiny
+
+		if destiny == "ZZZ" {
+			return i + 1, destiny
+		}
+	}
+	return len(instructions), destiny
+}
+
+func apply_step(directions map[string]Direction, step byte, start string) string {
+	if step == 'L' {
+		return directions[start].left
+	} else if step == 'R' {
+		return directions[start].right
+	} else {
+		err := fmt.Errorf("Instruction not recognized %b", step)
+		panic(err)
+	}
+}
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b uint64) uint64 {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func LCM(numbers []uint64) uint64 {
+	a := numbers[0]
+	b := numbers[1]
+	result := a * b / GCD(a, b)
+	for i := 2; i < len(numbers); i++ {
+		result = result * numbers[i] / GCD(result, numbers[i])
+	}
+	return result
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("The program requires the input file path as argument")
+	}
+	input := os.Args[1]
+	dat, err := os.ReadFile(input)
+	if err != nil {
+		panic("Input file not found")
+	}
+
+	parts := strings.Split(strings.Trim(string(dat), "\n"), "\n\n")
+	instructions := strings.Trim(parts[0], " ")
+	map_directions := strings.Split(parts[1], "\n")
+
+	directions := make(map[string]Direction)
+
+	starting_nodes := []string{}
+
+	for _, line := range map_directions {
+		parts := strings.Split(line, "=")
+		start := strings.Trim(parts[0], " ")
+		destinies := strings.Split(strings.Trim(parts[1], " ()"), ",")
+		left := strings.Trim(destinies[0], " ")
+		right := strings.Trim(destinies[1], " ")
+		directions[start] = Direction{left, right}
+		if start[len(start)-1] == 'A' {
+			starting_nodes = append(starting_nodes, start)
+		}
+	}
+
+	solution1 := 0
+	start := "AAA"
+	for {
+		steps, destiny := follow_instructions(directions, instructions, start)
+		solution1 += steps
+		start = destiny
+		if destiny == "ZZZ" {
+			break
+		}
+	}
+
+	destiny_nodes := make([]string, len(starting_nodes))
+
+	type Check struct {
+		column int
+		a      string
+		c      byte
+	}
+	checkpoints := make(map[Check]bool)
+	checkpoint_distances := make([]uint64, len(starting_nodes))
+
+	for k := 0; ; k++ {
+		step := instructions[k%len(instructions)]
+		for i, start := range starting_nodes {
+			destiny := apply_step(directions, step, start)
+			destiny_nodes[i] = destiny
+			if destiny[len(destiny)-1] == 'Z' {
+				if checkpoint_distances[i] == 0 {
+					check := Check{i, destiny, step}
+					_, ok := checkpoints[check]
+					if !ok {
+						checkpoint_distances[i] = uint64(k + 1)
+					}
+				}
+			}
+			starting_nodes[i] = destiny
+		}
+
+		every := true
+
+		for _, c := range checkpoint_distances {
+			if c == 0 {
+				every = false
+				break
+			}
+		}
+
+		if every {
+			break
+		}
+	}
+
+	solution2 := LCM(checkpoint_distances)
+
+	fmt.Println("solution 1:", solution1)
+	fmt.Println("solution 2:", solution2)
+}
