eeee0e3ce78f6ce0db084af068ed8bde23c77983
Guido Giuntoli <gagiuntoli@gmail.com>
2023-12-26 09:17:58 +0100

    day 19 - 2023

--- 2023/day19/main.go
+++ 2023/day19/main.go
@@ -71,6 +71,101 @@ func is_acepted(rulesMap map[string][]Rule, part Part) bool {
 	}
 }
 
+type Range struct {
+	min, max int
+}
+
+func (r *Range) empty() bool {
+	return r.min == -1 && r.max == -1
+}
+
+func ranges_size(ranges [4]Range) uint64 {
+	prod := uint64(1)
+	for _, range_ := range ranges {
+		prod *= uint64(range_.max - range_.min + 1)
+	}
+	return prod
+}
+
+func split_range(rule Rule, range_ Range) (Range, Range) {
+	a := range_.min
+	b := range_.max
+	v := rule.value
+	r := rule.relation
+	if r == ">" {
+		if b <= v {
+			return Range{-1, -1}, Range{a , b}
+		} else if a > v {
+			return Range{a, b}, Range{-1, -1}
+		}
+		return Range{v+1, b}, Range{a , v}
+	} else if r == "<" {
+		if b < v {
+			return Range{a, b}, Range{-1, -1}
+		} else if a >= v {
+			return Range{-1, -1}, Range{a, b}
+		}
+		return Range{a, v-1}, Range{v , b}
+	}
+	panic("the code should not have reach this point")
+}
+
+// This is a recursive function that takes ranges as inputs to compute the
+// number of possible accepted in those ranges
+func compute_accepted_ranges(rulesMap map[string][]Rule, name string, index int, ranges [4]Range) uint64 {
+	rules := rulesMap[name]
+	rule := rules[index]
+
+	for _, r := range ranges {
+		if r.empty() {
+			return 0
+		}
+	}
+
+	//cases A, R alone
+	if rule.part == "" {
+		if rule.result == "A" {
+			return ranges_size(ranges)
+		} else if rule.result == "R" {
+			return 0
+		} else {
+			return compute_accepted_ranges(rulesMap, rule.result, 0, ranges)
+		}
+	}
+
+	var range_index int
+	if rule.part == "x" {
+		range_index = 0
+	} else if rule.part == "m" {
+		range_index = 1
+	} else if rule.part == "a" {
+		range_index = 2
+	} else if rule.part == "s" {
+		range_index = 3
+	}
+
+	range_true, range_false := split_range(rule, ranges[range_index])
+
+	if rule.result == "A" {
+		// case a>8437:A
+		ranges[range_index] = range_true
+		c1 := ranges_size(ranges)
+		ranges[range_index] = range_false
+		c2 := compute_accepted_ranges(rulesMap, name, index+1, ranges)
+		return c1 + c2
+	} else if rule.result == "R" {
+		// case a>8437:R
+		ranges[range_index] = range_false
+		return compute_accepted_ranges(rulesMap, name, index+1, ranges)
+	} else {
+		ranges[range_index] = range_true
+		c1 := compute_accepted_ranges(rulesMap, rule.result, 0, ranges)
+		ranges[range_index] = range_false
+		c2 := compute_accepted_ranges(rulesMap, name, index+1, ranges)
+		return c1 + c2
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		panic("The program requires the input file path as argument")
@@ -138,8 +233,9 @@ func main() {
 			solution1 += part_sum(part)
 		}
 	}
+	ranges := [4]Range{{1, 4000}, {1, 4000}, {1, 4000}, {1, 4000}}
+	solution2 := compute_accepted_ranges(rules, "in", 0, ranges)
 
-	solution2 := 0
 	fmt.Println("solution 1:", solution1)
 	fmt.Println("solution 2:", solution2)
 }
--- 2023/day19/main_test.go
+++ 2023/day19/main_test.go
@@ -12,5 +12,5 @@ func TestMainFunc(t *testing.T) {
 	os.Args = []string{"./main", "./input.dat"}
 
 	solutions := strings.Split(utils.CaptureStdout(main), "\n")
-	utils.CheckSolution(t, solutions, "xxx", "xxx")
+	utils.CheckSolution(t, solutions, "397643", "132392981697081")
 }
